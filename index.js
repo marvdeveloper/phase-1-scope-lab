e581eeca1062cd830db385930b9ae8c85c34719b
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-04-01 13:25:41 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+// 1. Declare a global variable 'customerName'
+var customerName = 'bob';
+
+// 2. Function to uppercase 'customerName'
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// 3. Function to set 'bestCustomer'
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// 4. Function to overwrite 'bestCustomer'
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// 5. Declare 'leastFavoriteCustomer' constant
+const leastFavoriteCustomer = 'John';
+
+// 6. Function that attempts to change 'leastFavoriteCustomer' constant
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Jane'; // Error: cannot reassign constant
+}
